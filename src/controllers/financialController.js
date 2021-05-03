c383af519126edac2f27eb68886af93f6dce7157
lvduarte <lucvduarte@gmail.com>
2021-05-02 21:40:42 -0300

    correcao de dados ao pesquisar só por produto

--- src/controllers/financialController.js
+++ src/controllers/financialController.js
@@ -24,31 +24,39 @@ router.get("/", async (req, res) => {
                 $lt: endDate || new Date(year, Number(month) + 1, '01')
             };
         }
-
         const products = await Product.find(query);
+        delete query._id
+        query.product = _id;
         const sale = await Sale.find(query);
         let valueProducts = 0;
         let qtdProducts = 0;
+        let value = 0;
+
         products.map((el) => {
             qtdProducts += Number(el.qtd);
+            value = el.value;
             valueProducts += Number(el.value) * Number(el.qtd);
             return el;
         });
 
         let valueSale = 0;
         let qtdSale = 0;
+
         sale.map((el) => {
             qtdSale += Number(el.qtd);
             valueSale += Number(el.value) * Number(el.qtd);
             return el;
         });
-        const lucro = ((valueSale / valueProducts) - 1) * 100;
+
+        const totalProducts = qtdProducts + qtdSale;
+        const profit = ((valueSale / (valueProducts || (totalProducts * value))) - 1) * 100;
+
         return res.send({
-            totalProducts: qtdProducts,
-            valueProducts,
+            totalProducts: totalProducts,
+            valueProducts: valueProducts || (totalProducts * value),
             totalSale: qtdSale,
             valueSale,
-            lucro: lucro || 0,
+            profit: profit || 0,
         })
     } catch (error) {
         return res.status(400).send({ error: 'Error' });
