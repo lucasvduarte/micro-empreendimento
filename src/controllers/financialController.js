f6e1432fbf6581eb9edec4346f6f8f82d5223e79
lvduarte <lucvduarte@gmail.com>
2021-05-10 12:11:47 -0300

    correcao da parte financeira

--- src/controllers/financialController.js
+++ src/controllers/financialController.js
@@ -26,18 +26,19 @@ router.get("/", async (req, res) => {
         }
         const products = await Product.find(query);
         delete query._id
-        query.product = _id;
-        const sale = await Sale.find(query);
-        let valueProducts = 0;
+        if (_id) {
+            query.product = _id;
+        }
+        const sale = await Sale.find(query).populate('product');
         let qtdProducts = 0;
         let value = 0;
-
         products.map((el) => {
             qtdProducts += Number(el.qtd);
-            value = el.value;
-            valueProducts += Number(el.value) * Number(el.qtd);
+            value += el.value;
             return el;
         });
+
+        let valueProducts = qtdProducts * value;
 
         let valueSale = 0;
         let qtdSale = 0;
@@ -52,7 +53,7 @@ router.get("/", async (req, res) => {
         const profit = ((valueSale / (valueProducts || (totalProducts * value))) - 1) * 100;
 
         return res.send({
-            totalProducts: totalProducts,
+            totalProducts: qtdProducts,
             valueProducts: valueProducts || (totalProducts * value),
             totalSale: qtdSale,
             valueSale,
